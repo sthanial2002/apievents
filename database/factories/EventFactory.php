d838e923829e49cce2321db92f913a9598971238
Maurel OUSSOU <micronet@consultant.com>
2019-07-13 02:06:53 +0100

    Edit EventTableSeeder's file

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -13,7 +13,7 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\Event::class, function (Faker $faker) {
+$factory->define(App\Event::class, function (Faker\Generator $faker) {
 
 
     return [
